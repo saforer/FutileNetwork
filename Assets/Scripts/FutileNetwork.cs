bc9d07e7afc6e7e949f8e9a7e92c9de3f1d52d5e
saforer <saforer@gmail.com>
2016-08-19 04:11:09 -0400

    Box on screen better commit
    Signed-off-by: saforer <saforer@gmail.com>

---
+++ Assets/Scripts/FutileNetwork.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FutileNetwork : MonoBehaviour {
+    public static FutileNetwork instance;
+
+
+	// Use this for initialization
+	void Start () {
+        instance = this;
+
+
+        FutileParams fparams = new FutileParams(true, true, false, false);
+        fparams.AddResolutionLevel(240f, 1f, 1f, "");
+        fparams.origin = Vector2.zero;
+
+        Futile.instance.Init(fparams);
+        Futile.atlasManager.LoadAtlas("Atlases/atlas");
+
+        FSprite box = new FSprite("Box");
+        Futile.stage.AddChild(box);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
